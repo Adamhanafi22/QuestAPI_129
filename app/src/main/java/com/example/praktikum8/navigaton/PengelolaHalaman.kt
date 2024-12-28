f8e148d274b40b2907094e0fd73ebff5af6475a8
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-12-28 21:39:41 +0700

    membuat PengelolaHalaman

---
+++ app/src/main/java/com/example/praktikum8/navigaton/PengelolaHalaman.kt
@@ -0,0 +1,41 @@
+package com.example.praktikum8.navigaton
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum8.ui.home.screen.DestinasiEntry
+import com.example.praktikum8.ui.home.screen.DestinasiHome
+import com.example.praktikum8.ui.home.screen.EntryMhsScreen
+import com.example.praktikum8.ui.home.screen.HomeScreen
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
