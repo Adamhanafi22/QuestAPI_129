0f07b2ce1eb00b7b5c9aa6925e661d3a28f8df19
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-12-30 07:23:57 +0700

    membuat fungsi AtributDetailMhs untuk detailview

--- app/src/main/java/com/example/praktikum8/ui/home/screen/DetailView.kt
+++ app/src/main/java/com/example/praktikum8/ui/home/screen/DetailView.kt
@@ -1,14 +1,21 @@
 package com.example.praktikum8.ui.home.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.praktikum8.model.Mahasiswa
 import com.example.praktikum8.navigaton.DestinasiNavigasi
 
 object DestinasiDetail : DestinasiNavigasi {
@@ -19,7 +26,36 @@ object DestinasiDetail : DestinasiNavigasi {
 }
 
 @Composable
-fun DetailMhs(
+fun AtributDetailMhs(
+    mahasiswa: Mahasiswa
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            JudulDetailMhs(judul = "NIM", isinya = mahasiswa.nim)
+            Spacer(modifier = Modifier.padding(4.dp))
+            JudulDetailMhs(judul = "Nama", isinya = mahasiswa.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+            JudulDetailMhs(judul = "Alamat", isinya = mahasiswa.alamat)
+            Spacer(modifier = Modifier.padding(4.dp))
+            JudulDetailMhs(judul = "Jenis Kelamin", isinya = mahasiswa.jenisKelamin)
+            Spacer(modifier = Modifier.padding(4.dp))
+            JudulDetailMhs(judul = "Kelas", isinya = mahasiswa.kelas)
+            Spacer(modifier = Modifier.padding(4.dp))
+            JudulDetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
+        }
+    }
+}
+
+
+
+@Composable
+fun JudulDetailMhs(
     judul: String,
     isinya: String
 ) {
