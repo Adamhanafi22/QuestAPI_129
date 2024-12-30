fd9a9f69262f43efc34fe8e665ae86155c68496d
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-12-30 11:16:06 +0700

    menambahkan di penyedia view model

--- app/src/main/java/com/example/praktikum8/ui/home/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum8/ui/home/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum8.ui.home.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -10,6 +11,8 @@ object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
         initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { DetailMhsViewModel(createSavedStateHandle(),aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { UpdateMhsViewModel(createSavedStateHandle(),aplikasiMahasiswa().container.mahasiswaRepository) }
     }
 }
 
