895086e8d59e8633989e06026fe959b2598ec22f
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-12-30 09:54:27 +0700

    membuat fungsi DetailMhsView

--- app/src/main/java/com/example/praktikum8/ui/home/screen/DetailView.kt
+++ app/src/main/java/com/example/praktikum8/ui/home/screen/DetailView.kt
@@ -5,27 +5,83 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum8.model.Mahasiswa
 import com.example.praktikum8.navigaton.DestinasiNavigasi
+import com.example.praktikum8.ui.CostumeTopAppBar
+import com.example.praktikum8.ui.home.viewmodel.DetailMhsViewModel
 import com.example.praktikum8.ui.home.viewmodel.DetailUiState
+import com.example.praktikum8.ui.home.viewmodel.PenyediaViewModel
 
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "Detail"
     override val titleRes = "Detail Mahasiswa" // Implementasi wajib
     const val NIM = "nim"
-    val routeWithArg = "$route/{$NIM}"
+    val routesWithArg = "$route/{$NIM}"
+
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailMhsView(
+    nim: String,
+    modifier: Modifier = Modifier,
+    viewModel: DetailMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onEditClick: (String) -> Unit = {},
+    navigateBack:()->Unit,
+) {
+    Scaffold(
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiDetail.titleRes,
+                canNavigateBack = true,
+
+                navigateUp = navigateBack,
+                onRefresh = { viewModel.DetailMahasiswa() } // Trigger refresh action on refresh
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { onEditClick(nim) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa"
+                )
+            }
+        }
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
+
+        BodyDetailMhs(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            retryAction = { viewModel.DetailMahasiswa() }
+        )
+    }
+}
+
 
 @Composable
 fun BodyDetailMhs(
